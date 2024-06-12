4a01282ed642355ac3947eca371006032f8f4d2f
Agos <agozavia@gmail.com>
2024-06-12 02:13:06 +0200

    css added, js modified

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
  // Aquí tu código
+const boton = document.getElementById('agregar');
+const lista = document.getElementById('lista');
+
+
+
+boton.addEventListener('click', function(){
+    const nuevoElemento = prompt ('Añade un elemento a la lista');
+    if (nuevoElemento){
+        const añadoElemento = document.createElement('li'); 
+        añadoElemento.textContent = nuevoElemento; 
+        lista.appendChild(añadoElemento);
+    }
+})
+
+
+const titulo = document.querySelector('h1');
+const nuevoTitulo = "Lista de la compra";
+titulo.textContent= nuevoTitulo; 
