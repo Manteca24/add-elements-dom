a18a214250a045e97ff126c14f8d253dfd2c54b8
Agos <agozavia@gmail.com>
2024-06-12 11:16:29 +0200

    some advices from DATA

--- js/script.js
+++ js/script.js
@@ -1,8 +1,23 @@
  // Aquí tu código
-const boton = document.getElementById('agregar');
+console.log("hola");
+
+ const boton = document.getElementById('agregar');
 const lista = document.getElementById('lista');
 
+console.log(boton);
+console.log(lista);
+/* DATA recomienda hacer la función a parte y luego solo llamarla
+function addList { 
+     const nuevoElemento = prompt ('Añade un elemento a la lista');
+    if (nuevoElemento){
+        const añadoElemento = document.createElement('li'); 
+        añadoElemento.textContent = nuevoElemento; 
+        lista.appendChild(añadoElemento);
+    }
+}
 
+boton.addEventListener('click', addList());
+*/
 
 boton.addEventListener('click', function(){
     const nuevoElemento = prompt ('Añade un elemento a la lista');
@@ -10,10 +25,18 @@ boton.addEventListener('click', function(){
         const añadoElemento = document.createElement('li'); 
         añadoElemento.textContent = nuevoElemento; 
         lista.appendChild(añadoElemento);
-    }
-})
+    } else {
+        alert("por favor ingrese un valor válido")
+} 
+}
+)
 
+/*
+Otra manera de hacerlo MUY BUENA:
+en la función:
+lista.insertAdjacentHTML("beforeeend", ("<li>" + ciudad + "</li>")
 
+*/ 
 const titulo = document.querySelector('h1');
 const nuevoTitulo = "Lista de la compra";
 titulo.textContent= nuevoTitulo; 
